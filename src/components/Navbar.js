62a8fd135e1a1b1248a48e4cf5fdbaf00739ff14
vhaamb001 <vhamb001@odu.edu>
2024-11-08 21:45:10 +0300

    Navbar cahnges: added cards. still WIP though

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,123 +2,102 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import logo from './logo.png';
-import {useMediaQuery} from 'react-responsive';
+import { useMediaQuery } from 'react-responsive';
 
 function Navbar() {
   const [isSolutionsOpen, setIsSolutionsOpen] = useState(false);
   const [isSectorsOpen, setIsSectorsOpen] = useState(false);
   const [isServicesOpen, setIsServicesOpen] = useState(false);
   const [isPartnersOpen, setIsPartnersOpen] = useState(false);
-  // const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const solutionsSubMenuItems = [
-  { label: 'Audio Visual Integration', href: '/avIntegration' },
-  { label: 'Collaboration Solutions', href: '/collaborationSolutions' },
-  { label: 'Digital Media', href: '/digitalMedia' },
-  { label: 'Automation Solutions', href: '/automationSolutions' },
-  { label: 'Interactive Technology', href: '/interactiveTechnology' },
+    { label: 'Audio Visual Integration', href: '/avIntegration' },
+    { label: 'Collaboration Solutions', href: '/collaborationSolutions' },
+    { label: 'Digital Media', href: '/digitalMedia' },
+    { label: 'Automation Solutions', href: '/automationSolutions' },
+    { label: 'Interactive Technology', href: '/interactiveTechnology' },
   ];
 
-  const sectorsSubMenuItems =[
-    { label: 'Corporate', href: '/corporate'},
-    { label: 'Education', href: '/education'},
-    { label: 'Public Sector', href: '/publicSector'},
-    { label: 'Healthcare', href: '/healthcare'},
-    { label: 'Legal Collaboration', href: '/legalCollaboration'}
+  const sectorsSubMenuItems = [
+    { label: 'Corporate', href: '/corporate' },
+    { label: 'Education', href: '/education' },
+    { label: 'Public Sector', href: '/publicSector' },
+    { label: 'Healthcare', href: '/healthcare' },
+    { label: 'Legal Collaboration', href: '/legalCollaboration' },
   ];
 
   const servicesSubMenuItems = [
     { label: 'Unified Communications', href: '/unifiedCommunications' },
     { label: 'Training and Support', href: '/trainingAndSupport' },
-    { label: 'On-site Support', href: '/onSiteSupport'},
-    { label: 'Equipment Repair', href: '/equipmentRepair'},
-    { label: 'Leasing', href: '/leasing'},
-    { label: 'Video Conference Rental', href: '/videoConferenceRental'},    
+    { label: 'On-site Support', href: '/onSiteSupport' },
+    { label: 'Equipment Repair', href: '/equipmentRepair' },
+    { label: 'Leasing', href: '/leasing' },
+    { label: 'Video Conference Rental', href: '/videoConferenceRental' },
   ];
 
   const partnersSubMenuItems = [
-    { label: 'Panasonic', href: '/panasonic'},
-    { label: 'Crestron', href: '/crestron'},
-    { label: 'Optoma', href: '/optoma'},
-    { label: 'Lifesize', href: '/lifesize'},
-    { label: 'Barco', href: '/barco'},
-    { label: 'Samsung', href: '/samsung'},
-    { label: 'LG', href: '/lg'},
-    { label: 'Epson', href: '/epson'},
-    { label: 'Avaya', href: '/avaya'},
-    { label:'Kramer', href: '/kramer'},
-    { label: 'Vogel', href: '/vogel'},
-    { label: 'Extron', href: '/extron'}
+    { label: 'Panasonic', href: '/panasonic' },
+    { label: 'Crestron', href: '/crestron' },
+    { label: 'Optoma', href: '/optoma' },
+    { label: 'Lifesize', href: '/lifesize' },
+    { label: 'Barco', href: '/barco' },
+    { label: 'Samsung', href: '/samsung' },
+    { label: 'LG', href: '/lg' },
+    { label: 'Epson', href: '/epson' },
+    { label: 'Avaya', href: '/avaya' },
+    { label: 'Kramer', href: '/kramer' },
+    { label: 'Vogel', href: '/vogel' },
+    { label: 'Extron', href: '/extron' },
   ];
 
-  const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
-  const isLaptop = useMediaQuery({query: "(min-device-width: 1024px"});
-  const isTabletDevice = useMediaQuery({query: "(min-device-width: 768px)",});
-  const isDesktop = useMediaQuery({query: "(min-device-width: 1200px)",});
-  const isBigScreen = useMediaQuery({ query: "(min-device-width: 1201px )",});
+  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1024px)' });
 
   return (
     <nav className="navbar">
-    <div className="logo">
-      <Link to="/">
-        <img src={logo} alt="Company Logo" />
-      </Link>
-    </div>
-    <div className={`navbar-links ${isMobile ? 'mobile' : ''}`}></div>
-    <div className={`navbar-links ${isLaptop ? 'laptop' : ''}`}></div>
-    <div className={`navbar-links ${isTabletDevice ? 'tablet' : ''}`}></div>
-    <div className={`navbar-links ${isDesktop ? 'laptop' : ''}`}></div>
-    <div className={`navbar-links ${isBigScreen ? 'laptop' : ''}`}></div>
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Company Logo" />
+        </Link>
+      </div>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
 
-        {/* solutions menus and subs */}
         <li
           onMouseEnter={() => setIsSolutionsOpen(true)}
           onMouseLeave={() => setIsSolutionsOpen(false)}
         >
           <Link to="/solutions">Solutions</Link>
-          
-            <div >
-              {isSolutionsOpen && (
-                  // this place needs a container which wil hold everything else,
-              
-                <ul className="submenu" style={{backgroundColor:"red", height:"100vh", Width:"100%"}}>
-                  {solutionsSubMenuItems.map((item, index) => (
-                  
-                      <li key={index}><Link to={item.href}>{item.label}</Link></li>
-              
-                  ))}
-                </ul>
-              )}
-            </div>
-      
+          {isSolutionsOpen && (
+            <ul className={`submenu ${isDesktopOrLaptop ? 'submenu-fullwidth' : ''}`}>
+              {solutionsSubMenuItems.map((item, index) => (
+                <li key={index}><Link to={item.href}>{item.label}</Link></li>
+              ))}
+            </ul>
+          )}
         </li>
 
-        {/* servces menu and subs */}
         <li
           onMouseEnter={() => setIsServicesOpen(true)}
           onMouseLeave={() => setIsServicesOpen(false)}
         >
           <Link to="/services">Services</Link>
           {isServicesOpen && (
-            <ul className="submenu">
+            <ul className={`submenu ${isDesktopOrLaptop ? 'submenu-fullwidth' : ''}`}>
               {servicesSubMenuItems.map((item, index) => (
                 <li key={index}><Link to={item.href}>{item.label}</Link></li>
               ))}
             </ul>
           )}
         </li>
-        
-        {/* sectors menu and subs */}
+
         <li
           onMouseEnter={() => setIsSectorsOpen(true)}
           onMouseLeave={() => setIsSectorsOpen(false)}
         >
           <Link to="/sectors">Sectors</Link>
           {isSectorsOpen && (
-            <ul className="submenu">
+            <ul className={`submenu ${isDesktopOrLaptop ? 'submenu-fullwidth' : ''}`}>
               {sectorsSubMenuItems.map((item, index) => (
                 <li key={index}><Link to={item.href}>{item.label}</Link></li>
               ))}
@@ -126,14 +105,13 @@ function Navbar() {
           )}
         </li>
 
-        {/* partners menu and subs */}
         <li
           onMouseEnter={() => setIsPartnersOpen(true)}
           onMouseLeave={() => setIsPartnersOpen(false)}
         >
           <Link to="/partners">Partners</Link>
           {isPartnersOpen && (
-            <ul className="submenu">
+            <ul className={`submenu ${isDesktopOrLaptop ? 'submenu-fullwidth' : ''}`}>
               {partnersSubMenuItems.map((item, index) => (
                 <li key={index}><Link to={item.href}>{item.label}</Link></li>
               ))}
