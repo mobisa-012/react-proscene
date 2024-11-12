44ca2104fc1fef6e5c382ba6966495831a114e5e
vhaamb001 <vhamb001@odu.edu>
2024-11-12 08:27:45 +0300

    padding to home divs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Footer from "../components/Footer";
 import LogoCarousel from "../components/LogoCarousel";
+import SectorCards from "../components/SectorCards.js";
+import './Home.css';
 
 function Home() {
-    return(
-        <div>
-        <h1>This is ht ehome page</h1>
-        <LogoCarousel/>
-        <Footer/>
+    return (
+        <div className="home-container">
+        <h1>This is the home page</h1>
+
+        <div className="home-section">
+        <LogoCarousel />
+        </div>
+
+        <div className="home-section">
+        <SectorCards />
+        </div>
+
+        <Footer />
         </div>
     );
 }
