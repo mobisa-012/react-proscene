21177f4d14be8d9464f1e35da94792a1a75e5df6
mobisa-012 <mobisa12ivy@gmail.com>
2024-12-04 22:02:22 +0300

    added Solution component and changed layout a little. Still a WIP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,29 +5,32 @@ import SectorCards from "../components/SectorCards.js";
 import './Home.css';
 import Partners from "../components/Partners.js";
 import BookDemoToday from "../components/BookDemo.js";
+import SolutionsLayout from "../components/Solutions";
 
 
 function Home() {
     return (
         <div className="home-container">
-        <h1>This is the home page</h1>          
-
-        <div className="home-section">
-        <LogoCarousel />
+        <div>
+        <SolutionsLayout/>
         </div>
 
         <div className="home-section">
         <Partners/>
         </div> 
 
+        <div className="home-section">
+        <LogoCarousel /> 
+        </div>
+
         <div className="sector-section">
         <SectorCards />
-        </div>
+        </div>        
 
         <div className = "demo-section">
         <BookDemoToday/>
-        </div>           
-
+        </div> 
+        
         <Footer />
         </div>
     );
