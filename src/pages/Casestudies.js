07d6735074a630f423af2816150b229a88272b7c
mobisa-012 <mobisa12ivy@gmail.com>
2025-01-08 07:24:18 +0300

    case studies page progress

--- src/pages/Casestudies.js
+++ src/pages/Casestudies.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Footer from "../components/Footer";
+import BookDemoToday from "../components/BookDemo.js";
+import WhatCustomersSay from "../components/case-studies-components/WhatCustomersSay.js";
+import FeaturedStudies from "../components/case-studies-components/featured_studies.js";
 
 function Casestudies() {
     return(
-        <h1>
-        This is the case studies page</h1>
+        <div className = ''>        
+        <WhatCustomersSay/>
+        <div className= "pt-10 pb-10">
+        <h3 className="text-center text-2xl font-bold">Featured Case studies</h3>
+        </div>
+        <div className="">
+        <FeaturedStudies/>
+        </div> 
+        <BookDemoToday/>
+        <Footer/>
+        </div>
     );
 }
 
