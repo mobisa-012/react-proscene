1d3c5528b3fc8a54cde5e817714595135c64192c
mobisa-012 <mobisa12ivy@gmail.com>
2025-01-09 07:42:28 +0300

    solutions and sectors pages. Still WIP

--- src/pages/Sectors.js
+++ src/pages/Sectors.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React from 'react';
+import BookDemoToday from '../components/BookDemo';
+import Footer from '../components/Footer';
+import LogoCarousel from '../components/LogoCarousel';
 
-function Sectors() {
-    return(
-        <h1>
-        This is the sectors page</h1>
-    );
+function Sector() {
+  return (
+    <div className='p-0 pb-0'>    
+      <div className='pt-10 pb-10'>
+      <LogoCarousel /></div> 
+      <BookDemoToday/>
+      <Footer />
+    </div>  
+  );
 }
 
-export default Sectors;
+export default Sector
--- src/pages/Solutions.js
+++ src/pages/Solutions.js
@@ -2,14 +2,19 @@ import React from "react";
 import SolutionThatWorkForYou from "../components/SolutionWork.js";
 import BookDemoToday from "../components/BookDemo";
 import Footer from "../components/Footer";
+import LogoCarousel from '../components/LogoCarousel';
+import ProsceneGetYou from '../components/ProsceneGetsYou';
 
 function Solutions() {
     return(
-        <div className="">
-            <SolutionThatWorkForYou/>
-            <BookDemoToday/>
-            <Footer/>
-        </div>
+    <div className='p-0 pb-0'>    
+      <SolutionThatWorkForYou/>
+      <ProsceneGetYou/>
+      <div className='pt-10 pb-10'>
+      <LogoCarousel /></div> 
+      <BookDemoToday/>
+      <Footer />
+    </div> 
     );
 }
 
