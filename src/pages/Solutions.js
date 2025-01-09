156c07062ebf7c75ba8fbb5ab1b071900705004a
mobisa-012 <mobisa12ivy@gmail.com>
2025-01-09 14:54:46 +0300

    added a component to this page and its responsive

--- src/pages/Solutions.js
+++ src/pages/Solutions.js
@@ -6,11 +6,13 @@ import LogoCarousel from '../components/LogoCarousel';
 import ProsceneGetYou from '../components/ProsceneGetsYou';
 import ChooseYourFocus from "../components/solutions-components/ChooseFocus.js";
 import StartYourJorney from "../components/solutions-components/StartYourJourney.js";
+import MoreSection from "../components/Moresection.js";
 
 function Solutions() {
     return(
     <div className='p-0 pb-0'>    
       <SolutionThatWorkForYou/>
+      <MoreSection/>
       <ProsceneGetYou/>
       <div className = "pt-10 pb-10"><ChooseYourFocus/></div>
       <div className = "pt-10 pb-10"><StartYourJorney/></div>
