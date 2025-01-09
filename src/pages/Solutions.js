8cafdd531a8b3f3b572988c7b285eeef7d4890b9
mobisa-012 <mobisa12ivy@gmail.com>
2025-01-09 09:14:22 +0300

    update on Solutions page

--- src/pages/Solutions.js
+++ src/pages/Solutions.js
@@ -5,15 +5,16 @@ import Footer from "../components/Footer";
 import LogoCarousel from '../components/LogoCarousel';
 import ProsceneGetYou from '../components/ProsceneGetsYou';
 import ChooseYourFocus from "../components/solutions-components/ChooseFocus.js";
+import StartYourJorney from "../components/solutions-components/StartYourJourney.js";
 
 function Solutions() {
     return(
     <div className='p-0 pb-0'>    
       <SolutionThatWorkForYou/>
       <ProsceneGetYou/>
-      <div className = "pt-10 pb-10"><ChooseYourFocus/></div> 
-      <div className='pt-10 pb-10'>
-      <LogoCarousel /></div> 
+      <div className = "pt-10 pb-10"><ChooseYourFocus/></div>
+      <div className = "pt-10 pb-10"><StartYourJorney/></div>
+      <div className='pt-10 pb-10'><LogoCarousel /></div>
       <BookDemoToday/>
       <Footer />
     </div> 
