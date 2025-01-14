96067c69a00ba8cf402464b971156580c57a8489
mobisa-012 <mobisa12ivy@gmail.com>
2025-01-14 13:46:25 +0300

    reduced the x-axis width

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ const Footer = () => {
 
     return (
         <div className="footer">
-            <main className="main-container">
+            <main className="main-container max-w-7xl mx-auto">
                 <div className="logo" style={{margin:'10px 60px 10px 10px'}}>
                     <Link to="/">
                         <img src={logo} alt="Company Logo" />
@@ -89,13 +89,14 @@ const Footer = () => {
                 </div>                
             </main>
             
-            <div className="legal-links">
-            <div className="logo" style={{margin:'10px 60px 10px 10px'}}>
+            <div className="legal-links max-w-7xl mx-auto">
+                <div className="logo" style={{margin:'10px 60px 10px 10px'}}>
                     <Link to="/">
                         <img src={logo} alt="Company Logo" />
                     </Link>
-                </div>
+                </div>                
                 <div className="flex justify-center flex-col sm:flex-row" style={{gap:'clamp(2rem, 1.9322rem + 0.339vw, 2.25rem)'}}>
+                
                     <div className="flex flex-col sm:flex-row" style={{gap:'clamp(2rem, 1.9322rem + 0.339vw, 2.25rem)'}}>
                         <Link to="/terms-and-conditions" className="footer-link" style={{color:'white'}}>Terms & Conditions</Link>
                         <Link to="/privacy-policy" className="footer-link" style={{color:'white'}}>Privacy Policy</Link>
