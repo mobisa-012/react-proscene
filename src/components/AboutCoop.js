fd97148aa961dac64189f6c51ac488daa8ecb6df
mobisa-012 <mobisa12ivy@gmail.com>
2025-01-16 15:47:35 +0300

    added button fucntion

--- src/components/AboutCoop.js
+++ src/components/AboutCoop.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AboutCoop = () => {
+  const navigate  = useNavigate();
+  
+  const handleButtonClick = () => {
+      navigate("/case-studies");
+  }
   return (
     <div className="relative group w-full h-screen bg-[#fff] flex items-center justify-center">
       {/* Background Image with Overlay */}
@@ -28,7 +34,8 @@ const AboutCoop = () => {
       <div
         className="absolute top-3/4 -left-full transform -translate-y-1/2 mt-16
           text-white text-5xl font-semibold whitespace-nowrap transition-all duration-500 group-hover:left-10 mx-28 z-20">
-        <button className="bg-transparent text-white border-2 border-white px-4 py-2 rounded">
+        <button className="bg-transparent text-white border-2 border-white px-4 py-2 rounded"
+        onClick={handleButtonClick}>
           CASE STUDY -
         </button>
       </div>
--- src/components/AboutPs.js
+++ src/components/AboutPs.js
@@ -1,7 +1,13 @@
 import React from "react";
 import un from './assets/un.jpg';
+import { useNavigate } from "react-router-dom";
 
 const AboutPS= () => {
+    const navigate  = useNavigate();
+      
+      const handleButtonClick = () => {
+          navigate("/case-studies");
+      }
   return (
     <div className="relative group w-full h-screen bg-[#fff] flex items-center justify-center">
       {/* Background Image with Overlay */}
@@ -29,7 +35,8 @@ const AboutPS= () => {
       <div
         className="absolute top-3/4 -left-full transform -translate-y-1/2 mt-16
           text-white text-5xl font-semibold whitespace-nowrap transition-all duration-500 group-hover:left-10 mx-28 z-20">
-        <button className="bg-transparent text-white border-2 border-white px-4 py-2 rounded">
+        <button className="bg-transparent text-white border-2 border-white px-4 py-2 rounded"
+        onClick={handleButtonClick}>
           CASE STUDY -
         </button>
       </div>
