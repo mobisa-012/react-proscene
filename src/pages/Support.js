4022b8028f1eea7730f32ac8fb2c41f7eaa16631
mobisa-012 <mobisa12ivy@gmail.com>
2025-01-16 13:43:02 +0300

    support page: coming soon

---
+++ src/pages/Support.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Support() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-gray-800">
+                COMING SOON
+            </h1>
+        </div>
+    );
+}
+
+export default Support;
